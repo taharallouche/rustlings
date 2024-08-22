fe83f48a3465d9bfe339c607f0cf0390ae709eb0
taharallouche <tahar.allouche.pro@gmail.com>
2024-08-22 15:36:25 +0200

    feat: hashmaps exercices

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -17,7 +17,7 @@ struct Team {
 
 fn build_scores_table(results: &str) -> HashMap<&str, Team> {
     // The name of the team is the key and its associated struct is the value.
-    let mut scores = HashMap::new();
+    let mut scores: HashMap<&str, Team> = HashMap::new();
 
     for line in results.lines() {
         let mut split_iterator = line.split(',');
@@ -31,6 +31,22 @@ fn build_scores_table(results: &str) -> HashMap<&str, Team> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let default_team = Team {
+            goals_conceded: 0,
+            goals_scored: 0,
+        };
+
+        let team_1 = scores.entry(team_1_name).or_insert(default_team);
+        team_1.goals_conceded += team_2_score;
+        team_1.goals_scored += team_1_score;
+
+        let default_team = Team {
+            goals_conceded: 0,
+            goals_scored: 0,
+        };
+        let team_2 = scores.entry(team_2_name).or_insert(default_team);
+        team_2.goals_conceded += team_1_score;
+        team_2.goals_scored += team_2_score;
     }
 
     scores
