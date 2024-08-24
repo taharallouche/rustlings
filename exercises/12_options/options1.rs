933f6bd29c2a3e0188d97e612211b9e8d79fe702
taharallouche <tahar.allouche.pro@gmail.com>
2024-08-24 18:20:08 +0200

    feat: options exercies

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -3,7 +3,13 @@
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    if hour_of_day < 22 {
+        Some(5)
+    } else if hour_of_day <= 23 {
+        Some(0)
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -20,7 +26,7 @@ mod tests {
         // Option?
         let icecreams = maybe_icecream(12);
 
-        assert_eq!(icecreams, 5); // Don't change this line.
+        assert_eq!(icecreams, Some(5)); // Don't change this line.
     }
 
     #[test]
